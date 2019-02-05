e3820042d44b1c1a6dd418daa622bcce12c88709
herrybiscuit <herrybiscuit@gmail.com>
2019-02-05 18:56:57 -0500

    Updated algo

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ extern crate sdl2;
 extern crate specs;
 #[macro_use]
 extern crate specs_derive;
+extern crate rand;
+extern crate num;
 
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use rand::Rng;
+use num::clamp;
 
-const gravitonal_constant: f64 = 6.67e-11f64;
+const GRAVITONAL_CONSTANT: f64 = 6.67e-11f64;
 
 use specs::{Builder, Component, DispatcherBuilder, ReadStorage, System, VecStorage, World, WriteStorage, Entities, RunNow};
 
@@ -56,21 +60,21 @@ impl<'a> System<'a> for UpdateVel {
 
                 if let Some(other_mass) = other_mass {
                     if let Some(other_pos) = other_pos {
-                        // calculate distance between the two objects
-                        let dist = ((pos.x - other_pos.x).powf(2.0) + (pos.y - other_pos.y).powf(2.0)).sqrt();
-                        if dist != 0.0 {
-                            // calculate gravitonal force
-                            let grav = (gravitonal_constant * mass.mass * other_mass.mass) / dist.powf(2.0);
-                            // get angle between points
-                            let angle = (other_pos.x- pos.x).atan2(other_pos.y - pos.y);
-                            // deconstruct x and y values
-                            let change = angle.sin_cos();
-                            let x_change = change.0 * grav * 0.0000000001;
-                            let y_change = change.1 * grav * 0.0000000001;
+                        // cy = y2 - y1
+                        // cx = x2 - x1
+                        let cx = other_pos.x - pos.x;
+                        let cy = other_pos.y - pos.y;
 
-                            vel.x += x_change;
-                            vel.y += y_change;
-                        }
+                        // dist = 1.0/sqrt(cx^2 + cy^2 + min_dist)
+                        let dist = 1.0 / ((cx * cx) + (cy * cy) + 0.0001).sqrt();
+
+                        // f = G * m2 * m1 * dist * dist
+                        let f = GRAVITONAL_CONSTANT * mass.mass * other_mass.mass * dist * dist;
+
+                        // ay += cy * f
+                        // ax += cx * f
+                        vel.x += cx * f * 5.0f64;
+                        vel.y += cy * f * 5.0f64;
                     }
                 }
             }
@@ -114,24 +118,18 @@ pub fn main() {
     world.register::<Velocity>();
     world.register::<Mass>();
 
-    let zoom = (0.1, 0.1);
+    let mut zoom = (0.01, 0.01);
+    let mut offset = (0.0, 0.0);
 
-    /*world
-        .create_entity()
-        .with(Position { x: 0.0, y: 0.0 })
-        .with(Velocity { x: 0.0, y: 0.0 })
-        .with(Mass{ mass: 1.9e+20f64})
-        .build();*/
+    let mut rng = rand::thread_rng();
 
-    for x in 0..15{
-        for y in 0..15{
-            world
+    for _ in 0..1500{
+        world
                 .create_entity()
-                .with(Position { x: 50.0 * (0.0 + x as f64) - 375.0, y: 50.0 * (0.0 + y as f64) - 375.0 })
-                .with(Velocity { x: 0.001, y: 0.001 })
+                .with(Position { x: rng.gen_range(0.0, 1.0) * 1.0e+10f64, y: rng.gen_range(0.0, 1.0) * 1.0e+10f64})
+                .with(Velocity { x: rng.gen_range(-5.0, 5.0) * 1.0e+5f64, y: rng.gen_range(-5.0, 5.0) * 1.0e+5f64})
                 .with(Mass{ mass: 5.0e+10f64})
                 .build();
-        }
     }
 
     let mut dispatcher = DispatcherBuilder::new()
@@ -139,6 +137,11 @@ pub fn main() {
         .with(UpdatePos, "Update_pos", &["Update_vel"])
         //.with_thread_local(Render)
         .build();
+
+    let mut move_down = false;
+    let mut move_up = false;
+    let mut move_right = false;
+    let mut move_left = false;
 
     'running: loop {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
@@ -149,8 +152,49 @@ pub fn main() {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
+                Event::MouseWheel {y, ..} => {
+                    zoom.0 = clamp::<f64>((y as f64 * 0.001) + zoom.0, 0.0, 5.0);
+                    zoom.1 = clamp::<f64>((y as f64 * 0.001) + zoom.1, 0.0, 5.0);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), ..} => {
+                    move_down = true;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Up), ..} => {
+                    move_up = true;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), ..} => {
+                    move_right = true;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), ..} => {
+                    move_left = true;
+                },
+                Event::KeyUp { keycode: Some(Keycode::Down), ..} => {
+                    move_down = false;
+                },
+                Event::KeyUp { keycode: Some(Keycode::Up), ..} => {
+                    move_up = false;
+                },
+                Event::KeyUp { keycode: Some(Keycode::Right), ..} => {
+                    move_right = false;
+                },
+                Event::KeyUp { keycode: Some(Keycode::Left), ..} => {
+                    move_left = false;
+                },
                 _ => {}
             }
+        }
+
+        if move_down{
+            offset.1 -= 10.0;
+        }
+        if move_up{
+            offset.1 += 10.0;
+        }
+        if move_right{
+            offset.0 -= 10.0;
+        }
+        if move_left{
+            offset.0 += 10.0;
         }
 
         dispatcher.dispatch(&mut world.res);
@@ -166,7 +210,7 @@ pub fn main() {
             canvas.set_draw_color(Color::RGB(255, 248, 231));
             if let Some(vel) = vel {
                 let velocity = ((vel.x).powf(2.0) + (vel.y).powf(2.0)).sqrt();
-                let red_color = 255.0 * (velocity / 25.0);
+                let red_color = 255.0 * (velocity / 1e+7f64);
                 let red_color_clamp: u8;
                 if red_color > 255.0{
                     red_color_clamp = 255;
@@ -178,8 +222,10 @@ pub fn main() {
                 canvas.set_draw_color(Color::RGB(red_color_clamp, 248, 200));
             }
 
+            let copy_zoom = (zoom.0 * 0.000001, zoom.1 * 0.000001);
+
             if let Some(pos) = pos {
-                canvas.draw_point(sdl2::rect::Point::new(((pos.x + (400.0 / zoom.0)) * zoom.0) as i32, ((pos.y +  (300.0 / zoom.1)) * zoom.1) as i32));
+                canvas.draw_point(sdl2::rect::Point::new((offset.0 + (pos.x + (400.0 / copy_zoom.0)) * copy_zoom.0) as i32, (offset.1 + (pos.y +  (300.0 / copy_zoom.1)) * copy_zoom.1) as i32));
             }
         }
 
